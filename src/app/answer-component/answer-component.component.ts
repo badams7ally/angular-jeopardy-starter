23e44e5e3b9aaa3f7b1b58b90551f88fdc9a0845
MZS02B@ALLYIPOC.COM <MZS02B@ALLYIPOC.COM>
2018-06-26 10:49:53 -0400

    clean up

--- src/app/answer-component/answer-component.component.ts
+++ src/app/answer-component/answer-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-answer-component',
@@ -7,13 +7,10 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 })
 export class AnswerComponentComponent implements OnInit {
   @Input() questionInfo;
-  @Output() somethingHappened = new EventEmitter();
+  @Input() score;
   constructor() { }
 
   ngOnInit() {
   }
 
-  checkAnswerEvent() {
-     this.somethingHappened.emit();
-  }
 }
